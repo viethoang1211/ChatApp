4081682f15468fa3e654a80fbc6beab3b5a6f3d9
viethoang1211 <viethoang12112002@gmail.com>
2022-12-18 18:22:46 +0700

    test adding data multidex, cloud message

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -24,5 +28,28 @@ public class SignInActivity extends AppCompatActivity {
         binding.textCreateNewAccount.setOnClickListener(v ->
             startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
 
+//        test adding data
+//        binding.buttonSignIn.setOnClickListener(v-> addDataToFirestore());
+
     }
+
+    // test add data
+    /*
+    private void addDataToFirestore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String,Object> data = new HashMap<>();
+        data.put("first_name","Lac");
+        data.put("last_name", "Viet");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(),"Data inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception-> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+    */
+
+
 }
